3f1e1603be110c0d11e7e95862adcaf64c33ffb6
Garrett Graham <ganderson7197@icloud.com>
2016-02-09 16:15:55 -0500

    working on the portfolio. following along with a tutorial in order to gain better footing on javascript as the guy uses js. learned something new about css transitions and got the dummy image for a skill. learned how to input html into a js variable and then iterate over the images of skills that will then be appended to the class in the skills section. need to get more images for the skills i have

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+var skills = '<div class="col-md-3"><img src="./img/%data%.png" alt="JS" class="img-responsive" /></div>';
 
+var skills_array = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+
+var skills_new = ''; /* initialize with an empty array*/
+
+for (var i = 0; i < skills_array.length; i+= 1) {
+  skills_new = skills.replace('%data%', skills_array[i]);
+  $('.skills-images').append(skills_new);
+}
